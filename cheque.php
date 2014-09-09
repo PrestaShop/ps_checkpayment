090a0fef9f3d7b00a21b08b6a890827c5cd67ccf
Damien Metzger <damien.metzger@prestashop.com>
2014-09-09 13:23:27 +0200

    // Changed cheque condition: unpaid out of stock display a success message

--- cheque.php
+++ cheque.php
@@ -153,7 +153,7 @@ class Cheque extends PaymentModule
 			return;
 
 		$state = $params['objOrder']->getCurrentState();
-		if ($state == Configuration::get('PS_OS_CHEQUE') || $state == Configuration::get('PS_OS_OUTOFSTOCK'))
+		if (in_array($state, array(Configuration::get('PS_OS_CHEQUE'), Configuration::get('PS_OS_OUTOFSTOCK'), Configuration::get('PS_OS_OUTOFSTOCK_UNPAID'))))
 		{
 			$this->smarty->assign(array(
 				'total_to_pay' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
