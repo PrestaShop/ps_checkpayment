b7641d59a9d7c378c84bca79b0d63712624c3475
tchauviere <thibaud.chauviere@prestashop.com>
2015-05-18 18:24:23 +0200

    // Prepared upgrades files for revert hook displayPaymentApi

--- upgrade/upgrade-2.5.8.php
+++ upgrade/upgrade-2.5.8.php
@@ -27,7 +27,11 @@
 if (!defined('_PS_VERSION_'))
 	exit;
 
-function upgrade_module_2_5_7($module)
+function upgrade_module_2_5_8($module)
 {
+	$hook_to_remove_id = Hook::getIdByName('advancedPaymentApi');
+	if ($hook_to_remove_id) {
+		$module->unregisterHook((int)$hook_to_remove_id);
+	}
 	return true;
 }
