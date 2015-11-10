6e9e977663960854e1eefca9a708c8371ec94990
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-11-10 17:28:44 +0100

    // Version updated to v2.7.1

--- cheque.php
+++ cheque.php
@@ -40,7 +40,7 @@ class Cheque extends PaymentModule
 	{
 		$this->name = 'cheque';
 		$this->tab = 'payments_gateways';
-		$this->version = '2.7.0';
+		$this->version = '2.7.1';
 		$this->author = 'PrestaShop';
 		$this->controllers = array('payment', 'validation');
 		$this->is_eu_compatible = 1;
