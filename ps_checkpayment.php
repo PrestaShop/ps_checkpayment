535d53b47519b5e97f80ebf55a58d60ab852e620
Michael Dekker <git@michaeldekker.nl>
2016-09-05 22:43:27 +0200

    MO: Bail out when PS version is lower than 1.7

--- ps_checkpayment.php
+++ ps_checkpayment.php
@@ -24,11 +24,15 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }
+
+if (version_compare(_PS_VERSION_, '1.7.0.0', '<')) {
+    return;
+}
+
+use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 
 class Ps_Checkpayment extends PaymentModule
 {
