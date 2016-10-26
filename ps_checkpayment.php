1a5d3d5fc1a8b0e4f93c29e930eb5fdafc2b7917
Maxime Biloé <maxime.biloe@prestashop.com>
2016-10-26 15:42:54 +0200

    // Version updated to v1.0.7

--- ps_checkpayment.php
+++ ps_checkpayment.php
@@ -43,7 +43,7 @@ class Ps_Checkpayment extends PaymentModule
     {
         $this->name = 'ps_checkpayment';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.6';
+        $this->version = '1.0.7';
         $this->author = 'PrestaShop';
         $this->controllers = array('payment', 'validation');
 
