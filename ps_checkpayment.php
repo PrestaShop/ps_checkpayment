c2a7b63ca8ea9b7f54d7776e59f26956a90fc2df
Thomas Nabord <thomas.nabord@prestashop.com>
2018-11-05 11:30:19 +0000

    Bump version to 2.0.4

--- ps_checkpayment.php
+++ ps_checkpayment.php
@@ -43,7 +43,7 @@ class Ps_Checkpayment extends PaymentModule
     {
         $this->name = 'ps_checkpayment';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.3';
+        $this->version = '2.0.4';
         $this->author = 'PrestaShop';
         $this->controllers = array('payment', 'validation');
 
