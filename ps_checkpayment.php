f2ff99a53d862d1f471530b7b74020eda9d3ae48
okom3pom <contact@okom3pom.com>
2021-04-08 15:08:16 +0200

    Don't display warning if module disabled

--- ps_checkpayment.php
+++ ps_checkpayment.php
@@ -66,7 +66,7 @@ class Ps_Checkpayment extends PaymentModule
         $this->confirmUninstall = $this->trans('Are you sure you want to delete these details?', [], 'Modules.Checkpayment.Admin');
         $this->ps_versions_compliancy = ['min' => '1.7.1.0', 'max' => _PS_VERSION_];
 
-        if ((!isset($this->checkName) || !isset($this->address) || empty($this->checkName) || empty($this->address))) {
+        if ((!isset($this->checkName) || !isset($this->address) || empty($this->checkName) || empty($this->address)) && 1 == $this->active) {
             $this->warning = $this->trans('The "Payee" and "Address" fields must be configured before using this module.', [], 'Modules.Checkpayment.Admin');
         }
         if (!count(Currency::checkPaymentCurrencies($this->id))) {
